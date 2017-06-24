5664535c3179c8d66bd143aac0e9d914b657630c
odulzaides <dulzaides.oscar@gmail.com>
2017-06-23 20:33:18 -0700

    Navbar dopdown needs work

--- imports/ui/nav.jsx
+++ imports/ui/nav.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
 
 class Nav extends React.Component {
-    constructor() {
+    constructor(){
         super();
 
         this.handleHover();
     }
 
     // Drop menu down
-    handleHover() {
+    handleHover(){
         let elem = $('.nav li');
         let dropDown = $('.dropdown');
-        elem.hover(() => {
+        elem.hover(()=> {
             dropDown.toggleClass('show-dropdown');
         });
     }
@@ -21,34 +21,19 @@ class Nav extends React.Component {
                 <div className="nav-brand">
                     <h2>Brand</h2>
                 </div>
-                <div className="nav">
-                    <ul>
-                        <li className="sub-menu-parent">
-                            <a href="#">Menu Item 1</a>
-                            <ul className="sub-menu">
-                                <li><a href="#">Sub Item 1</a></li>
-                                <li><a href="#">Sub Item 2</a></li>
-                                <li><a href="#">Sub Item 3</a></li>
-                                <li><a href="#">Sub Item 4</a></li>
-                            </ul>
+                <nav>
+                    <ul className="main-menu">
+                        <li ><a href="#">Blog</a></li>
+                        <li><a href="#">Store</a>
+                        <ul>
+                            <li><a href="#">Clothing</a></li>
+                            <li><a href="#">Gear</a></li>
+                            <li><a href="#">Services</a></li>
+                        </ul>
                         </li>
-                        <li className="sub-menu-parent"><a href="#">Menu Item 2</a>
-                            <ul className="sub-menu">
-                                <li><a href="#">Sub Item 1</a></li>
-                                <li><a href="#">Sub Item 2</a></li>
-                                <li><a href="#">Sub Item 3</a></li>
-                                <li><a href="#">Sub Item 4</a></li>
-                                <li><a href="#">Sub Item 5</a></li>
-                                <li><a href="#">Sub Item 6</a></li>
-                            </ul>
-                        </li>
-                        <li className="sub-menu-parent"><a href="#">Menu Item 3</a>
-                            <ul className="sub-menu">
-                                <li><a href="#">Sub Item 1</a></li>
-                                <li><a href="#">Sub Item 2</a></li>
-                            </ul></li>
+                        <li><a href="#">Contact</a></li>
                     </ul>
-                </div>
+                </nav>
             </div>
         )
     }
